504323680c27dc8d21162062be2f682f1d7307ba
Barak Saidoff <barak.saidoff@gmail.com>
2020-08-21 13:26:52 -0400

    setting up to push to Heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-
+require('dotenv').config()
 // const Loan = require("./models/Loan")
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/snco-calculator-backend');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const bodyParser= require('body-parser')
 
